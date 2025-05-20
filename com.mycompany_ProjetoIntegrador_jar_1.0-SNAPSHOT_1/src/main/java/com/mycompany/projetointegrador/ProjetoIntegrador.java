e004ecbd5498e0b883bbcf1a11adcc1b60d1609f
kauazin12389 <kauafercar.37@gmail.com>
2025-05-20 08:32:08 -0300

    Arrumando Git

---
+++ com.mycompany_ProjetoIntegrador_jar_1.0-SNAPSHOT_1/src/main/java/com/mycompany/projetointegrador/ProjetoIntegrador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetointegrador;
+
+/**
+ *
+ * @author 25.00768-3
+ */
+public class ProjetoIntegrador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
